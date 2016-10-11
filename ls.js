3a58a77c3e6ab424d44df1fd58711f378894b756
njain7 <njain1@walmartlabs.com>
2016-10-11 16:00:39 -0700

    Adding ls file

---
+++ ls.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+require('./helper')
+let fs = require('fs').promise;
+let path = require('path');
+let {dir} = require('yargs')
+  .default('dir', __dirname)
+  .argv;
+
+async function ls(rootPath) {
+  console.log('Executing ls function...');
+  let fileNames = await fs.readdir(rootPath);
+  for (let fileName of fileNames) {
+    let filePath = path.join(rootPath, fileName);
+    fs.stat(filePath, function(err, stats) {
+      try{
+        if (stats&&stats.isFile()) {
+          console.log(fileName);
+        }else if(stats && stats.isDirectory()){
+          console.log("directory : "+fileName);
+        }
+      }catch(e){
+        console.log("error");
+      }
+    });
+
+  }
+}
+async function main() {
+  await ls(dir);
+}
+main();
